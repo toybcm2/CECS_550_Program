7bc39da10aaeaa8a00491e2778c771f374e1d388
brand <brand@DESKTOP-R6J1N8M>
2017-03-09 17:52:28 -0500

    Add project files.

---
+++ CECS_550_Program/Models/Menu_Items.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace CECS_550_Program
+{
+    public class Menu_Items
+    {
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        public Symbol SymbolIcon
+        {
+            get;
+            set;
+        }
+
+        public Type NavigateTo
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ CECS_550_Program/Views/Menu_System.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CECS_550_Program
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Menu_System : Page
+    {
+        public Menu_System()
+        {
+            this.InitializeComponent();
+            PageFrame.Navigate(typeof(Login_Page));
+            PageFrame.Navigated += PageFrame_Navigated;
+            SystemNavigationManager.GetForCurrentView().BackRequested += OnBackRequested;
+        }
+
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainSplitView.IsPaneOpen = !MainSplitView.IsPaneOpen;
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            PageFrame.Navigate(typeof(Home_Page));
+        }
+
+        private void UserButton_Click(object sender, RoutedEventArgs e)
+        {
+            PageFrame.Navigate(typeof(User_Settings_Page));
+        }
+
+        private void AboutButton_Click(object sender, RoutedEventArgs e)
+        {
+            PageFrame.Navigate(typeof(About_Page));
+        }
+
+        private void PageFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                ((Frame)sender).CanGoBack ?
+                AppViewBackButtonVisibility.Visible :
+                AppViewBackButtonVisibility.Collapsed;
+        }
+
+        private void OnBackRequested(object sender, BackRequestedEventArgs e)
+        {
+            if (PageFrame.CanGoBack)
+            {
+                e.Handled = true;
+                PageFrame.GoBack();
+            }
+        }
+    }
+}

c43d7a9b9e9bbfbe0408c1b8984fa9cab0dfebbd
toybcm2 <toybcm2@gmail.com>
2017-04-17 14:36:05 -0400

    Inital rtc connection handling done

---
+++ CECS_550_Program/RTC/MyBuffer.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Windows.Storage.Streams;
+
+namespace CECS_550_Program.RTC
+{
+    class MyBuffer
+    {
+        private IBuffer buffer;
+
+        public MyBuffer(IBuffer buf)
+        {
+            this.buffer = buf;
+        }
+
+        public MyBuffer(byte[] byteArr)
+        {
+            this.buffer = byteArr.AsBuffer();
+        }
+
+        public MyBuffer(string s)
+        {
+            byte[] byteArr = Encoding.UTF8.GetBytes(s.ToCharArray());
+            this.buffer = byteArr.AsBuffer();
+        }
+
+        public byte[] AsByteArray()
+        {
+            return WindowsRuntimeBufferExtensions.ToArray(this.buffer);
+        }
+
+        public string AsString()
+        {
+            return Encoding.UTF8.GetString(this.AsByteArray());
+        }
+
+        public IBuffer Buffer
+        {
+            get { return buffer; }
+        }
+    }
+}
--- CECS_550_Program/Views/Event_Page.xaml.cs
+++ CECS_550_Program/Views/Event_Page.xaml.cs
@@ -13,6 +13,8 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using CECS_550_Program.Common;
+using CECS_550_Program.RTC;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -24,6 +26,7 @@ namespace CECS_550_Program
     public sealed partial class Event_Page : Page
     {
         private MediaPlayer _mediaPlayer;
+        RTCHandler rtc = new RTCHandler(new RTCConnectionObject());
 
         public Event_Page()
         {
@@ -31,12 +34,62 @@ namespace CECS_550_Program
             System.Uri manifestUri = new Uri("http://amssamples.streaming.mediaservices.windows.net/49b57c87-f5f3-48b3-ba22-c55cfdffa9cb/Sintel.ism/manifest(format=m3u8-aapl)");
             _mediaPlayer = new MediaPlayer();
             _mediaPlayer.Source = MediaSource.CreateFromUri(manifestUri);
-            _mediaPlayer.Play();
+            //_mediaPlayer.Play();
+
+            this.DataContext = new EventViewModel();
+            Task.Run(async () => {
+                string connecting;
+                try
+                {
+                    UpdateChat(connecting = await rtc.ConnectAsync(null, new ConnectionInformationObject(Guid.NewGuid(), "toybcm@gmail.com", false)));
+                }
+                catch (Exception e)
+                { }
+            });
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(Login_Page));
+        }
+
+        private void MessageArea_KeyUp(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        {
+            if(e.Key == Windows.System.VirtualKey.Enter && MessageArea.Text != "")
+            {
+                rtc.SendAsync(MessageArea.Text);
+                MessageArea.Text = "";
+            }
+        }
+
+        private async void UpdateChat(string connecting)
+        {
+            try
+            {
+                EventViewModel data;
+
+                Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    data = DataContext as EventViewModel;
+                    data.Chat += connecting + "\n";
+                    DataContext = data;
+                }).AsTask().Wait();
+                //DataContext.Chat += connecting + "\n";
+
+                while (true)
+                {
+                    string s = await rtc.ReadAsync();
+
+                    Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    {
+                        data = DataContext as EventViewModel;
+                        data.Chat += s + "\n";
+                        DataContext = data;
+                    }).AsTask().Wait();
+                }
+            }
+            catch (Exception e)
+            { }
         }
     }
 }
